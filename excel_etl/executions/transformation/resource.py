b62b0b713cea7d282cd3f1d39e87fb6d0378e768
Ilan Kosteniov <ikosteniov97@gmail.com>
2020-03-05 16:50:55 +0200

    added mission

--- excel_etl/executions/transformation/resource.py
+++ excel_etl/executions/transformation/resource.py
@@ -1,16 +1,26 @@
+from pandas import DataFrame
+
 from excel_etl.executions.transformation.transformation import Transformation
 
+columns = {
+    ""
+}
 
 class ResourceTransformation(Transformation):
     def __init__(self, sheet_name, column_schema):
         super().__init__(sheet_name, column_schema)
 
     def transform(self, df_dict):
-        return self.transform(df_dict)
+        return super().transform(df_dict)
         # TODO: Add the specific transformation for every resource.
+
+    def exchange_df(self, df: DataFrame):
+        # TODO: change the df according to the specific relevant schema
+        pass
 
     def get_new_schema(self, transformed_data):
         # TODO: Use the prefix to determine what object it is.
         return {
-            "res_id": transformed_data.get('')
+            "res_id": transformed_data.get(''),
+            "res"
         }
