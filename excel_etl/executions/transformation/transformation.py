ea0114ce6acdd7668f9ed0f199a7e5375e71ed34
Ilan Kosteniov <ikosteniov97@gmail.com>
2020-03-06 20:33:31 +0200

    added phone

--- excel_etl/executions/transformation/transformation.py
+++ excel_etl/executions/transformation/transformation.py
@@ -1,12 +1,16 @@
 import json
 import locale
 from datetime import datetime
+from typing import Dict, List
+
+from pandas import DataFrame
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 hebrew_to_english_keys = {
     "אימייל": "email",
-    "דוא\"ל": "email"
+    "דוא\"ל": "email",
+    "טלפון": "phone"
 }
 
 
@@ -31,20 +35,25 @@ def parse_bool_type(value):
     return None
 
 
+def remove_insertion_time(df: DataFrame):
+    return df.drop(columns=[c for c in df.columns.values.tolist() if "_add_date" in c])
+
+
 class Transformation(object):
-    def __init__(self, sheet_name, column_schema):
+    def __init__(self, sheet_name: str, column_schema: Dict):
         self.sheet_name = sheet_name
         self.column_schema = column_schema
 
-    def transform(self, df):
+    def transform(self, df: DataFrame):
+        df = remove_insertion_time(df)
         df_dict = df.to_dict('records')
         return [{self.sheet_name: self.change_df(df_dict)}]
 
-    def change_df(self, df_dict):
+    def change_df(self, df_dict: List[Dict]):
         return [{key: self.handle_type(key, self.column_schema.get(key), value) for key, value in
                  df_dict_item.items()} for df_dict_item in df_dict]
 
-    def handle_type(self, column_name, column_type, column_value):
+    def handle_type(self, column_name: str, column_type: str, column_value: str):
         if column_type == str:
             return column_value
         elif column_type == int:
@@ -54,6 +63,7 @@ class Transformation(object):
         elif column_type == bool:
             return parse_bool_type(column_value)
         elif column_type == datetime:
+            # TODO: handle date parsing
             return column_value
             # return parser.parse(column_value)
         else:
